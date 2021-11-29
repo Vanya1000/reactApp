e5f2f3684adff8774e792522d9ad77241cef47fc
Vanya1000 <Vanya1000@yandex.ru>
2021-11-29 18:27:04 +0300

    fead: add import/export

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+let b = 234;
+let c = 345;
+let a = () => b * c;//?
+
 
 const App = () => {
   return (
     <div className="App">
-      <div className="hi">
-        <ul className="todo">
-          <li>html</li>
-          <li>css</li>
-          <li>scss</li>
-          <li>js</li>
-        </ul>
-      </div>
+      <Header />
+      <Tehnologies />
     </div>
   );
 }
 
+const Tehnologies = () => {
+  return (
+    <ul className="tehologies">
+      <li><a href="#"> html</a></li>
+      <li><a href="#"> css</a></li>
+      <li><a href="#"> scss</a></li>
+      <li><a href="#"> js</a></li>
+      <li><a href="#"> JSX</a></li>
+      <li><a href="#"> react</a></li>
+    </ul>
+  );
+}
+
+const Header = () => {
+  return (
+    <div className="num">
+      <p>123</p>
+      <p>456</p>
+      <p>789</p>
+    </div>
+  );
+}
 export default App;
