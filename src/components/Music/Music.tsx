076e1e6893369520c0149b354ba0bc81d4d8b264
Vanya1000 <Vanya1000@yandex.ru>
2022-03-13 12:37:58 +0300

    feat: compleated way

--- src/components/Music/Music.tsx
+++ src/components/Music/Music.tsx
@@ -6,7 +6,7 @@ import s from './Music.module.css';
 const Music: React.FC = () => {
 	return (
 		<div>
-			<Github/>
+			<Github />
 		</div>
 	)
 }
@@ -22,10 +22,17 @@ type SearchResult = {
 	items: SearchUserType[]
 }
 
+type UserType = {
+	login: string
+	id: number
+	avatar_url: string
+	followers: number
+}
 
 const Github = () => {
 	const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null)
-	const [users, setUsers] = useState<SearchUserType[]>([])
+	const initialSearchState = 'it'
+	const [searchTerm, setSearchTerm] = useState(initialSearchState)
 
 	useEffect(() => {
 		if (selectedUser) {
@@ -33,39 +40,150 @@ const Github = () => {
 		}
 	}, [selectedUser])
 
-	useEffect(() => {
-		axios
-		.get<SearchResult>('https://api.github.com/search/users?q=Vanya')
-		.then(res => {
-			setUsers(res.data.items)
-		})
-	}, [])
-
 	return <div>
 		<Row>
 			<Col flex={1}>
 				<div>
-					<div>
-						<input placeholder="search" /> <button>find</button>
-					</div>
-					<ul>
-						{users.map(u => <li key={u.id} className={selectedUser === u ? s.selected : ''} 
-						onClick={() => {
-							setSelectedUser(u)
-							}}>
-							{u.login}
-							</li>)}
-					</ul>
+					<Search value={searchTerm} onSubmit={(value: string) => { setSearchTerm(value) }} />
+					<button onClick={() => { setSearchTerm(initialSearchState) }}>reset</button>
+					<UsersList searchTerm={searchTerm} selectedUser={selectedUser} onUserSelect={setSelectedUser} />
 				</div>
 			</Col>
 			<Col flex={9}>
-				<div>
-					<h2>Username</h2>
-					<div>Details</div>
-				</div>
+				<UserDetails selectedUser={selectedUser} />
 			</Col>
 		</Row>
-		
-		
+	</div>
+}
+
+type SearchPropsType = {
+	value: string
+	onSubmit: (value: string) => void
+}
+
+
+const Search: React.FC<SearchPropsType> = ({ onSubmit, value }) => {
+	const [tempSearch, setTempSearch] = useState(value)
+
+	useEffect(() => {// для синхронизации. Когда в родительск компоненте происходит сброс, дочерняя синхронизируется.
+		setTempSearch(value)
+	}, [value])
+
+	return <div>
+		<input placeholder="search" value={tempSearch} onChange={(e) => { setTempSearch(e.currentTarget.value) }} />
+		<button
+			onClick={() => {
+				onSubmit(tempSearch)
+			}}
+		>find</button>
+	</div>
+}
+
+type UserListPropsType = {
+	searchTerm: string
+	selectedUser: SearchUserType | null
+	onUserSelect: (user: SearchUserType) => void
+}
+
+const UsersList: React.FC<UserListPropsType> = ({ searchTerm, selectedUser, onUserSelect }) => {
+
+	const [users, setUsers] = useState<SearchUserType[]>([])
+
+	useEffect(() => {
+		axios
+			.get<SearchResult>(`https://api.github.com/search/users?q=${searchTerm}`)
+			.then(res => {
+				setUsers(res.data.items)
+			})
+	}, [searchTerm])
+
+	return <div>
+		<ul>
+			{users.map(u => <li key={u.id} className={selectedUser === u ? s.selected : ''}
+				onClick={() => {
+					onUserSelect(u)
+				}}>
+				{u.login}
+			</li>)}
+		</ul>
+	</div>
+}
+
+
+const START_TIMER_SECONDS = 10;
+
+
+type TimerPropsType = {
+	seconds: number
+	onChange: (actualSeconds: number) => void
+	timerKey: string
+}
+
+const Timer: React.FC<TimerPropsType> = ({ seconds, onChange, timerKey }) => {
+
+	const [timer, setTimer] = useState(seconds)
+
+	useEffect(() => {
+		setTimer(seconds)
+	}, [seconds])
+
+	useEffect(() => {
+		onChange(timer)
+	}, [timer])
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setTimer((prev) => prev - 1)
+		}, 1000)// через prev получаем актуальный state обходя проблему замыкания
+		return () => { clearInterval(intervalId)}
+	}, [timerKey])// делаем установку интервала
+
+
+	return <div>
+		{timer}
+	</div>
+}
+
+
+
+
+
+
+type PropsType = {
+	selectedUser: SearchUserType | null
+}
+
+const UserDetails: React.FC<PropsType> = ({ selectedUser }) => {
+	const [userDetails, setUserDetails] = useState<UserType | null>(null)
+	const [seconds, setSeconds] = useState(START_TIMER_SECONDS)
+
+
+
+	useEffect(() => {
+		if (!!selectedUser) {
+			axios
+				.get<UserType>(`https://api.github.com/users/${selectedUser.login}`)
+				.then(res => {
+					setSeconds(START_TIMER_SECONDS)
+					setUserDetails(res.data)
+				})
+		}
+	}, [selectedUser])
+
+	useEffect(() => {
+		if (seconds < 0) {
+			setUserDetails(null)// но таймер не умер, так как это асинхрон живущий в браузере. И происходит утечка памяти.
+		}
+	}, [seconds])
+
+	return <div>
+
+		{userDetails && <div>
+			<Timer seconds={seconds} onChange={setSeconds} timerKey={userDetails.id.toString()}/>
+			<img src={userDetails.avatar_url} alt="" />
+			<br />
+			<h2>Username</h2>
+			{userDetails.login}, followers:{userDetails.followers}
+		</div>}
 	</div>
 }
